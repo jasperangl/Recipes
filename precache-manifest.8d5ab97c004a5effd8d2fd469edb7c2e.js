e0cbc799daf552a31b924f4fbe3fc85e6046add1
jasperangl <49351257+jasperangl@users.noreply.github.com>
2020-06-16 21:47:13 +0200

    Updates

---
+++ precache-manifest.8d5ab97c004a5effd8d2fd469edb7c2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3475e808f3f241dfd6c88238f7bc7221",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "8c9673a4870298659c2c",
+    "url": "/Recipes/static/css/main.01fb1e8f.chunk.css"
+  },
+  {
+    "revision": "2e38cd0ac4450e1ba1fe",
+    "url": "/Recipes/static/js/2.261b142c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipes/static/js/2.261b142c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9673a4870298659c2c",
+    "url": "/Recipes/static/js/main.ad011ec3.chunk.js"
+  },
+  {
+    "revision": "6f554dbbfd4fdc5c08ca",
+    "url": "/Recipes/static/js/runtime-main.3414d26c.js"
+  },
+  {
+    "revision": "63ae1930b87c77682a5213d2b0f9e947",
+    "url": "/Recipes/static/media/JA-logo-3.63ae1930.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bcedfddd494b455193d0aade1994a4bd.js"
+  "/Recipes/precache-manifest.8d5ab97c004a5effd8d2fd469edb7c2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Recipes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
