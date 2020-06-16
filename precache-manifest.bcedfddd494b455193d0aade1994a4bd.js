4143bef5c90f559b550fb1ec490df2ea11b28784
jasperangl <49351257+jasperangl@users.noreply.github.com>
2020-06-16 21:07:18 +0200

    Updates

---
+++ precache-manifest.bcedfddd494b455193d0aade1994a4bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9801bf1aa79f7b52b481a3b8cb5d6dff",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70101287af3c3e38469f",
+    "url": "/portfolio/static/css/main.774060b7.chunk.css"
+  },
+  {
+    "revision": "ad3da7611f782839ab1b",
+    "url": "/portfolio/static/js/2.ec7e76a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ec7e76a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70101287af3c3e38469f",
+    "url": "/portfolio/static/js/main.59ccf717.chunk.js"
+  },
+  {
+    "revision": "06409dda1b5d8ff51a0a",
+    "url": "/portfolio/static/js/runtime-main.8855b594.js"
+  },
+  {
+    "revision": "63ae1930b87c77682a5213d2b0f9e947",
+    "url": "/portfolio/static/media/JA-logo-3.63ae1930.png"
+  }
+]);
